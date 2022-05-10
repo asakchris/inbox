d7b05070c21c9c1c184e4cc7e5c8c836bf053367
Kamaraj Christopher <k.appadurai@spglobal.com>
2022-05-09 20:31:24 -0500

    Upgraded Solidity version to 0.8.9

--- deploy.js
+++ deploy.js
@@ -1,10 +1,10 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
-    'replace it with your own mnemonic',
-    'replace it with your own https URL to Infura Ethereum node'
+    'YOUR_MNEMONIC',
+    'YOUR_INFURA_URL'
 );
 const web3 = new Web3(provider);
 
@@ -12,8 +12,8 @@ const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
         .send({ from: accounts[0], gas: '1000000' });
     console.log('Contract deployed to', result.options.address);
 
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,7 +2,7 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const { interface, bytecode } = require('../compile');
+const { abi, evm } = require('../compile');
 
 // create web3 instance to connect to ganache using ganache provider
 const web3 = new Web3(ganache.provider());
@@ -16,8 +16,8 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     // use one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-                .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+    inbox = await new web3.eth.Contract(abi)
+                .deploy({ data: evm.bytecode.object, arguments: [INITIAL_MESSAGE] })
                 .send({ from: accounts[0], gas: '1000000' });
     //console.log(inbox);
 });
